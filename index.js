0b1df868d35499b4b4b42cdae7958ba78c31e308
Jonathan <jlassam@gmail.com>
2022-06-16 10:44:53 -0700

    working on an index page that serves up an elevator simulator

--- index.js
+++ index.js
@@ -2,10 +2,82 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-let database = [];
+let elevatorRaw = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+let elevatorProcessed  = {floors: elevatorRaw.length, current_Floor: 3};
+
+function display(height, location, width = 1, depth = 1){
+    let asciiDisplay = "";
+    for(let i = height; i > 0; i--){
+        if(i == location){
+            asciiDisplay = asciiDisplay + "<br> |&nbsp X &nbsp | ";
+        }
+        else{
+            asciiDisplay = asciiDisplay + "<br> | &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp | ";
+        }
+    }
+    return asciiDisplay
+}
+
+function elevatorButton(floor){
+    elevatorProcessed.current_Floor = floor;
+    let html = defineHTML();
+    res.send(html);
+}
+
+function buttonGen(elevatorFloors){
+    let buttonHTML = "";
+    let counter = 0;
+        for(let i of elevatorFloors){
+            if(counter%3 == 0){
+                buttonHTML = buttonHTML + "<br> <button onclick = elevatorButton(i)>" + i + "</button>";
+            }
+            else if(counter%3 == 1){
+                buttonHTML = buttonHTML + "&nbsp&nbsp <button onclick = elevatorButton(i)>" + i + "</button>";
+            }
+            else{
+                buttonHTML = buttonHTML + "&nbsp&nbsp <button onclick = elevatorButton(i)>" + i + "</button>";
+            }
+        counter++;
+        }
+    return buttonHTML
+    }
+
+function defineHTML(){
+    myHTML = `
+    <html>
+    <head>
+        <title>Elevator</title>
+        <style>
+    
+        h1{
+            font-size: 150px;
+            font-family: "Helvetica", "Arial", sans;
+        }
+    
+        </style>
+    </head>
+    <body>
+        <div>
+            <h1>Elevator Floor ${elevatorProcessed.current_Floor}</h1>
+            <p>
+            ${display(elevatorProcessed.floors, elevatorProcessed.current_Floor)}
+            </p>
+        </div>
+        ${buttonGen(elevatorRaw)}
+    </body>
+    </html>
+    `
+    return myHTML;
+}
+
+
+app.get('/', (req,res) => {
+        
+    let html = defineHTML();    
+    
+    res.send(html);
+    
 })
 
 app.get('/borp', (req, res) => {
